86e486841473244704b3a58168ec58c44817dafe
Himanshu Naudiyal <harishnaudiyal2@gmail.com>
2026-01-22 10:26:43 +0530

    Done

---
+++ DAY-01/TypeConversion.js
@@ -0,0 +1,19 @@
+// string conversion
+
+let value = true;
+console.log(typeof value); 
+value = String(value);
+console.log(typeof value);
+
+// numeric conversion
+
+let str= "123";
+console.log(typeof str);
+let num = Number(str);
+console.log(typeof num);
+// boolean conversion
+
+let n = 1;
+console.log(typeof n);
+let bool = Boolean(n);
+console.log(typeof bool);
